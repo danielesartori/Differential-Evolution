dcfaaf826aea2264c5f50d52188f0f4fb5aa64c7
Daniele Sartori <32380162+DanSartori@users.noreply.github.com>
2020-03-06 09:59:40 +0800

    Add files via upload

---
+++ eval_obj_fun.m
@@ -0,0 +1,13 @@
+%Evaluate the objective function 
+
+
+%function [f,objf_out]=eval_obj_fun(X,objf_var,objf_k)
+
+%Some code depending on X
+    
+%Objective function evaluation
+%f = function of X, objf_var and objf_k
+    
+%Output objective function information
+%objf_out = struct of f and the parameters used for the evaluation of f
+
